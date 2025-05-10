a350a2bd8dd7540c20fa86aae5f907c258a62889
alfreddserraa <asega2725gmail.com>
2025-05-10 23:47:05 +0200

    Versión inicial del sitio con HTML, CSS y JS

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('formulario').addEventListener('submit', function (e) {
+    e.preventDefault(); // evita que la página se recargue al enviar
+    alert('Gracias por tu mensaje. Te contactaremos pronto.');
+  });  
