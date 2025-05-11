c47717f8c6d293394ff6e8d3faaf939526d3c31e
alfreddserraa <asega2725gmail.com>
2025-05-11 21:50:31 +0200

    eliminacion

--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('formulario').addEventListener('submit', function (e) {
-    e.preventDefault(); // evita que la página se recargue al enviar
-    alert('Gracias por tu mensaje. Te contactaremos pronto.');
-  });  
